e44a935c94c0cf4d2be4147756df00a9350fdc9e
Thilina <thilina8madushanka@gmail.com>
2025-01-17 01:52:25 +0530

    profile navigate

---
+++ client/src/Components/ProfileComponents/ProfileUserDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileUserDetails = () => {
+  return (
+    <div>
+      <p> profile user details</p>
+    </div>
+  );
+};
+
+export default ProfileUserDetails;
--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoReorderThree } from "react-icons/io5";
 import { menu } from "./SidebarConfig";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const [activeTab, setActiveTab] = useState();
+  const navigate = useNavigate();
+
+  const handleTabClick = (title) => {
+    setActiveTab(title);
+    if (title === "Profile") {
+      navigate("/username");
+    } else if (title === "Home") {
+      navigate("/");
+    }
+  };
+
   return (
     //when page scrolling side bat doesnot scroll
     <div className="stickey top-0 h-[100vh]">
-      <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-col justify-between h-full px-10">
         <div>
           <div className="pt-10">
             <img
@@ -16,18 +29,34 @@ const Sidebar = () => {
             />
           </div>
           <div className="mt-10">
+            {/* handle click methods to navigate */}
             {menu.map((item) => (
-              <div className="flex items-center mb-5 cursor-pointer text-lg">
-                {item.icon}
-                <p>{item.title}</p>
+              <div
+                onClick={() => handleTabClick(item.title)}
+                className="flex items-center mb-5 cursor-pointer text-lg"
+              >
+                {/* when we click the tab it become bold and effect left */}
+                {activeTab === item.title ? item.activeIcon : item.icon}
+                <p
+                  className={`${
+                    activeTab === item.title ? "font-bold" : "font-semibold"
+                  }`}
+                  // style={{
+                  //   minWidth: "100px", // Ensures consistent width
+                  //   textAlign: "left", // Aligns text properly
+                  // }}
+                >
+                  {item.title}
+                </p>
               </div>
             ))}
           </div>
         </div>
-      </div>
-      <div className="flex items-center cursor-pointer pb-10">
-        <IoReorderThree />
-        <p className="ml-5">more</p>
+        {/* this dev tag mention tne more icon */}
+        <div className="flex items-center cursor-pointer pb-10">
+          <IoReorderThree className="text-2xl" />
+          <p className="ml-5">more</p>
+        </div>
       </div>
     </div>
   );
