d2a28c9a82a9639967b09aa06b7777024dc746c6
Thilina <thilina8madushanka@gmail.com>
2025-01-21 17:04:25 +0530

    like and comment

--- client/src/Components/Post/PostCard.jsx
+++ client/src/Components/Post/PostCard.jsx
@@ -85,6 +85,11 @@ const PostCard = () => {
             )}
           </div>
         </div>
+
+        <div className="w-full py-2 px-5">
+          <p>10 likes</p>
+          <p className="opacity-50 py-2 cursor-pointer">view all 10 Comments</p>
+        </div>
       </div>
     </div>
   );
