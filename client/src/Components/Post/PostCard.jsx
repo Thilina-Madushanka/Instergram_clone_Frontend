d410d915b600cd6a73c41ec8a36cc604f1696775
Thilina <thilina8madushanka@gmail.com>
2025-01-24 22:20:33 +0530

    comment section

--- client/src/Components/Post/PostCard.jsx
+++ client/src/Components/Post/PostCard.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BsBookmark, BsThreeDots, BsBookmarkFill } from "react-icons/bs";
+import {
+  BsBookmark,
+  BsThreeDots,
+  BsBookmarkFill,
+  BsEmojiSmile,
+} from "react-icons/bs";
 import "./PostCard.css";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
@@ -89,6 +94,17 @@ const PostCard = () => {
         <div className="w-full py-2 px-5">
           <p>10 likes</p>
           <p className="opacity-50 py-2 cursor-pointer">view all 10 Comments</p>
+        </div>
+
+        <div>
+          <div>
+            <BsEmojiSmile />
+            <input
+              className="commentInput"
+              type="text"
+              placeholder="Add a Comment..."
+            />
+          </div>
         </div>
       </div>
     </div>
