5466de46c4e7b03bf8e6ad50f6720a8fc7cc5e4c
Thilina <thilina8madushanka@gmail.com>
2025-01-25 21:09:09 +0530

    introducing chakra ui -  need to add index.js

---
+++ client/src/Components/Comments/CommentsModal.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CommentsModal = () => {
+  return (
+    <div>
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody> </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+export default CommentsModal;
