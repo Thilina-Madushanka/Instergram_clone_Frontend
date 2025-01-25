1d7564b52ec77b15f2e2d0bfa4d094584a6dd52b
Thilina <thilina8madushanka@gmail.com>
2025-01-25 13:00:29 +0530

    implemtnt comments

--- client/src/Components/Post/PostCard.jsx
+++ client/src/Components/Post/PostCard.jsx
@@ -91,13 +91,13 @@ const PostCard = () => {
           </div>
         </div>
 
-        <div className="w-full py-2 px-5">
-          <p>10 likes</p>
-          <p className="opacity-50 py-2 cursor-pointer">view all 10 Comments</p>
+        <div className="flex items-center space-x-10 w-full py-2 px-5">
+          <p>10 like</p>
+          <p className="opacity-50 py-2 cursor-pointer">View All 10 Comments</p>
         </div>
 
-        <div>
-          <div>
+        <div className="border border-t w-full">
+          <div className="flex items-center space-x-5 w-full py-2 px-5">
             <BsEmojiSmile />
             <input
               className="commentInput"
