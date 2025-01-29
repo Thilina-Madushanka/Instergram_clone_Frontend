8cf4fae70f77ac4ea535955e6118f11ed6375ef8
Thilina <thilina8madushanka@gmail.com>
2025-01-30 01:44:08 +0530

    search bar

--- client/src/Components/SearchComponents/SearchComponents.jsx
+++ client/src/Components/SearchComponents/SearchComponents.jsx
@@ -20,3 +20,31 @@ const SearchComponents = () => {
 };
 
 export default SearchComponents;
+
+// import "./SearchComponents.css";
+// import SearchUserCard from "./SearchUserCard";
+
+// const SearchComponents = ({ onClose }) => {
+//   return (
+//     <div className="SearchContainer">
+//       <div className="px-3 pb-5">
+//         <h1 className="flex justify-between text-xl pb-5">
+//           Search
+//           <button onClick={onClose} className="text-red-500 text-lg">
+//             ✖
+//           </button>
+//         </h1>
+
+//         <input className="searchInput" type="text" placeholder="Search..." />
+//       </div>
+//       <hr />
+//       <div className="searchResults">
+//         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+//           <SearchUserCard key={index} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SearchComponents;
